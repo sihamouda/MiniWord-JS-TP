32bdcc5aa2ecd6d7671f7fda862cb625e730a6c9
sihamouda <anis.c.hamouda@gmail.com>
2022-03-13 23:24:32 +0100

    working

--- MiniWord.js
+++ MiniWord.js
@@ -1,13 +1,11 @@
 var btn=document.querySelector("#button");
 btn.onclick= function (e){
     e.preventDefault();
-    
-    var color=document.querySelector("#color");
-    var width=document.querySelector("#width");
-    var font=document.querySelector("#font");
-    
-    document.querySelector("p").style.color=color;
-    document.querySelector("p").style.fontsize=width+'px';
-    document.querySelector("p").style.font=font;
+    let color=document.querySelector("#color");
+    let width=document.querySelector("#width");
+    let font=document.querySelector("#font");
+    let paragraph=document.querySelector("p");
+       
+    paragraph.setAttribute("style","color: "+color.value+"; font-size: "+width.value+"px; font: "+font.value+";");
     
 };
